71ca1f56a1abab4094918c4d562b2d313cc69e92
AvaNoilhetas <a.noilhetas@hotmail.fr>
2021-02-04 01:52:20 +0100

    [+]: Add url to get comics by ch. id

--- routes/comics.js
+++ routes/comics.js
@@ -19,4 +19,21 @@ router.get("/comics", async (req, res) => {
   }
 });
 
+router.get("/comics/:id", async (req, res) => {
+  try {
+    axios
+      .get(
+        `https://lereacteur-marvel-api.herokuapp.com/comics/${req.params.id}?apiKey=${process.env.API_SECRET_KEY}`
+      )
+      .then(response => {
+        res.status(200).json(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  } catch (error) {
+    res.status(400).json({ message: error });
+  }
+});
+
 module.exports = router;
