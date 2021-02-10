56f3ef2e6f9e1783d6831a52e1f4fb8d4177c5b1
AvaNoilhetas <a.noilhetas@hotmail.fr>
2021-02-10 01:45:21 +0100

    [~]: Fix vulnerability

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ require("dotenv").config();
 const charactersRoute = require("./routes/characters");
 const comicsRoute = require("./routes/comics");
 
-app.use(cors(), charactersRoute, comicsRoute);
+let corsOptions = {
+  origin: process.env.FRONT_WEB_SITE
+};
+
+app.disable("x-powered-by");
+app.use(cors(corsOptions), charactersRoute, comicsRoute);
 
 app.all("*", (req, res) => {
   res.status(400).json({ message: "not found" });
